6a71e32be0835042a04aed0e22bf07a4c73f8219
hdesumo <honoredesumo@yahoo.com>
2025-08-25 17:47:05 +0200

    chore: initial commit

--- config/config.js
+++ config/config.js
@@ -1,29 +1,19 @@
-// config/config.js
 require('dotenv').config();
 
-module.exports = {
-  development: {
-    // utile si tu veux tester en local plus tard
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASS || null,
-    database: process.env.DB_NAME || "postgres",
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: process.env.DB_SSL === "true" ? { require: true, rejectUnauthorized: false } : false
-    },
-    logging: false
-  },
+const common = {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {}
+};
 
-  // >>> utilisé sur Railway (Shell + container)
-  production: {
-    use_env_variable: "DATABASE_URL", // Railway la fournit automatiquement
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: false }
-    },
-    logging: false
-  }
+if (process.env.PGSSLMODE) {
+  common.dialectOptions.ssl = { require: true, rejectUnauthorized: false };
+}
+
+module.exports = {
+  development: { ...common, url: process.env.DATABASE_URL },
+  test:        { ...common, url: process.env.DATABASE_URL },
+  production:  { ...common, url: process.env.DATABASE_URL }
 };
 
